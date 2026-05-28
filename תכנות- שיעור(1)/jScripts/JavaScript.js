7f6d8bd7bdeff12dc4e6e5a1736a433c073c5e4f
hadarrrmeir <hadarmmm5@gmail.com>
2026-05-28 09:32:47 +0300

    project
    project

---
+++ תכנות- שיעור(1)/jScripts/JavaScript.js
@@ -0,0 +1,10 @@
+﻿function culculateNum(){
+let num1= parseInt(document.getElementById("text1").value);
+let num2= parseInt(document.getElementById("text2").value);
+let num3= parseInt(document.getElementById("text3").value);
+
+
+let avgNum= (num1+num2+num3) / 3;
+
+document.getElementById("result").innerHTML= 'ממוצע הציונים הוא:' + avgNum.toString();
+}
